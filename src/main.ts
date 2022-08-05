87cca5daddb8b2ff77d4d0c3e92565edf2fc8f39
Henry Tran <kevin.tran@vmodev.com>
2022-08-05 16:21:14 +0700

    new

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.enableCors();
-  app.use(cors({
-    origin: '*'
-  }));
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  // });
 
   const config = new DocumentBuilder()
     .setTitle('Chat bot VMO')
